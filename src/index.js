7c8d2ef65f12f6f025455fa655cb59313b5d24c4
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-06-30 11:14:28 -0300

    Subindo o projeto

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Routes from './routes';
+import "./config/StatusBarConfig";
+
+// Isso é a mesma coisa
+const App = () => <Routes />;
+
+export default App;
+
+// Disso, porem esse aqui possui funcionanidades que não serão usadas nesse caso
+// class App extends Component { 
+//     render() {
+//         return <Routes />
+//     }
+// }
+
